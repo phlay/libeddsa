cda973d4ea6c49d7204c08dd94dbb9b31dece0cb
Franz Gregor <franz.gregor@tu-dresden.de>
2017-02-15 13:32:15 +0100

    added c++ guards

--- lib/sha256.h
+++ lib/sha256.h
@@ -3,10 +3,14 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct {
 	uint32_t	state[8];
 	uint64_t	count;
+
 	uint8_t		buffer[64];
 	size_t		fill;
 } sha256ctx;
@@ -15,5 +19,7 @@ void sha256_init(sha256ctx *ctx);
 void sha256_add(sha256ctx *ctx, const uint8_t *data, size_t len);
 void sha256_done(sha256ctx *ctx, uint8_t out[32]);
 
-
+#ifdef __cplusplus
+}
+#endif
 #endif
